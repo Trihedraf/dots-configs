9955c936fc7dfba278a66837e8bfe34852a7dfa5
Trihedraf <forrest@mailfsh.com>
2026-08-09 06:32:12 -0700

    fix fontInstall.sh

--- scripts/fontInstall.sh
+++ scripts/fontInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir -p "$HOME/.local/share/fonts/commitmono-nerd" "$HOME/.local/share/fonts/meslo-lgs-nerd" "$HOME/.local/share/fonts/ubuntu-mono-nerd"
-curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CommitMono.tar.xz | tar -xJvf - -C "$HOME/.local/share/fonts/commitmono-nerd" "CommitMonoNerdFontMono-*.otf"
+curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CommitMono.tar.xz | tar -xJvf - --wildcards -C "$HOME/.local/share/fonts/commitmono-nerd" "CommitMonoNerdFontMono-*.otf"
 printf "CommitMono Nerd Font has been installed.\n"
-curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.tar.xz | tar -xJvf - -C "$HOME/.local/share/fonts/meslo-lgs-nerd" "MesloLGSNerdFontMono-*.ttf"
+curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.tar.xz | tar -xJvf - --wildcards -C "$HOME/.local/share/fonts/meslo-lgs-nerd" "MesloLGSNerdFontMono-*.ttf"
 printf "Meslo Nerd Font has been installed.\n"
-curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/UbuntuMono.tar.xz | tar -xJvf - -C "$HOME/.local/share/fonts/ubuntu-mono-nerd" "UbuntuMonoNerdFontMono-*.ttf"
+curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/UbuntuMono.tar.xz | tar -xJvf - --wildcards -C "$HOME/.local/share/fonts/ubuntu-mono-nerd" "UbuntuMonoNerdFontMono-*.ttf"
 printf "Ubuntu Mono Nerd Font has been installed.\n"
